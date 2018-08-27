310a980af0f9e0c68c294053a8c40be12e916a52
BJLU <banluckdock@gmail.com>
2018-08-27 12:15:11 +0300

    PerformVideo

---
+++ ConsoleApp1/ConsoleApp1/CreateVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace VideoPart
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce Path of the your Video -> ");
+            var pathVideo = Console.ReadLine();
+
+            Console.WriteLine("Introduce first point (format 00:00:00) -> ");
+            var firstPoint = Console.ReadLine();
+
+            Console.WriteLine("Introduce second point (format 00:00:00) -> ");
+            var secondPoint = Console.ReadLine();
+
+            PerformingVideo(pathVideo, firstPoint, secondPoint);
+        }
+
+        private static void PerformingVideo(string path, string firstPoint, string secondPoint)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirectory.ToString());
+            Process newVideo = new Process();
+            newVideo.StartInfo.FileName = "ffmpeg";
+            newVideo.StartInfo.Arguments = "-i " + path + " -ss " + firstPoint + " -t " + secondPoint + " -c copy " + currentDirectory + "\\OutPutVideo.mp4";
+            newVideo.StartInfo.RedirectStandardError = true;
+            newVideo.StartInfo.UseShellExecute = false;
+            newVideo.Start();
+        }
+    }
+}
